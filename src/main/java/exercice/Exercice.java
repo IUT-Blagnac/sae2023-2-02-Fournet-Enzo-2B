8916c87e419b605813d86537d10079470d5186da
Fournet Enzo <enzofournet31@gmail.com>
2023-06-18 11:53:48 +0200

    compte rendu

--- src/main/java/exercice/Exercice.java
+++ src/main/java/exercice/Exercice.java
@@ -1,1 +1,57 @@
-package exercice;import java.util.ArrayList;import java.util.List;public class Exercice {public static List<String> solution(String t, List<Character> o) {List<String> l1 = new ArrayList<>();String s1 = "";for (int i = 0; i < t.length(); i++){char c = t.charAt(i);if (Character.isLetter(c)) {s1 += c;} else if (c == '\'') {s1+=' ';} else {if (!s1.isEmpty()) {l1.add(s1);s1 = "";}}}if (!s1.isEmpty()) {l1.add(s1);}non(l1, o, 0, l1.size() - 1);return l1;}private static void non(List<String> l2, List<Character> o, int d, int f) {if (d < f) {int p = prt(l2, o, d, f);non(l2, o, d, p - 1);non(l2, o, p + 1, f);}}private static int prt(List<String> l3, List<Character> non, int stp, int nnf) {String jhtfygjfjyg = l3.get(nnf);int i = stp - 1;for (int j = stp; j < nnf; j++) {if (jsp(l3.get(j), jhtfygjfjyg, non) <= 0) {i++;oui(l3, i, j);}}oui(l3, i + 1, nnf);return i + 1;}    private static int jsp(String int2, String int89, List<Character> gana){int m1 = Math.min(int2.length(), int89.length());for (int i = 0; i < m1; i++) {char gez = int2.charAt(i);char cpeutetre = int89.charAt(i);if (gez != cpeutetre) {return getI(gez, gana) - getI(cpeutetre, gana);}}return int2.length() - int89.length();}private static int getI(char c, List<Character> jsp) {for (int i = 0; i < jsp.size(); i++) {if (jsp.get(i) == c) {return i;}}return -1;}private static void oui(List<String> l4, int lettre, int jafar) {String non = l4.get(lettre);l4.set(lettre, l4.get(jafar));l4.set(jafar, non);}}
+package exercice;
+
+public class Exercice {
+    public static List<String> solution(String str, List<Character> ordre) {
+        // La chaîne de caractères est divisée en mots à l'aide de l'espace comme
+        // séparateur
+        String[] words = str.split(" ");
+
+        // Pour chaque mot
+        for (String word : words) {
+            // Pour chaque caractère du mot
+            for (char c : word.toCharArray()) {
+                // Pour chaque caractère dans la liste d'ordre
+                for (Character ordreChar : ordre) {
+                    // Si le caractère du mot est dans la liste d'ordre
+                    if (c == ordreChar) {
+                        // Récupère son indice dans la liste d'ordre
+                        int index = ordre.indexOf(ordreChar);
+                    }
+                }
+            }
+        }
+
+        // Le tableau de mots est trié en utilisant un comparateur personnalisé
+        Arrays.sort(words, new Comparator<String>() {
+            @Override
+            // La méthode de comparaison pour le comparateur personnalisé est définie
+            public int compare(String o1, String o2) {
+                // Récupère la position de la première lettre de chaque mot dans la liste
+                // 'ordre'
+                int index1 = ordre.indexOf(o1.charAt(0));
+                int index2 = ordre.indexOf(o2.charAt(0));
+
+                // Si les deux lettres ne figurent pas dans 'ordre', elles sont considérées
+                // comme égales
+                if (index1 == -1 && index2 == -1)
+                    return 0;
+                // Si la première lettre figure dans 'ordre' et la deuxième non, la première est
+                // placée en premier
+                else if (index1 != -1 && index2 == -1)
+                    return -1;
+                // Si la première lettre ne figure pas dans 'ordre' et la deuxième si, la
+                // deuxième est placée en premier
+                else if (index1 == -1)
+                    return 1;
+                // Si les deux lettres figurent dans 'ordre', leur position dans 'ordre'
+                // détermine leur ordre
+                else
+                    return Integer.compare(index1, index2);
+            }
+        });
+
+        // Retourne la liste de mots triés
+        return Arrays.asList(words);
+    }
+
+}
